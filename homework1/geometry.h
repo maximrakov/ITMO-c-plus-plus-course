8216877d64835cfe9f173c6a64adbc4be6ae660c
maximrakov <78982680+maximrakov@users.noreply.github.com>
2021-03-05 11:51:41 +0300

    Update geometry.h

--- homework1/geometry.h
+++ homework1/geometry.h
@@ -1,183 +1,79 @@
-#include <cmath>
-//we want cpp file
-//we need spaces and go indenation
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
 class Point{
     public:
-        Point(int x_ = 0,int y_ = 0)
-        : x(x_), y(y_) {
-        }
-        Point(Point const &pnt)
-        : x(pnt.x),y(pnt.y) {
-        }
-        Point& operator=(const  Point& other){
-            if(&other == this){
-                return *this;
-            }
-            x = other.x;
-            y = other.y;
-            return *this;
-        }
-        
-        int getX()const{
-            return x;
-        }
-        int getY()const{
-            return y;
-        }
+        Point(int x_ = 0,int y_ = 0);
+        Point(Point const &pnt);
+        Point& operator=(const  Point& other);
+        int getX()const;
+        int getY()const;
         
     private:
         int x,y;
 };
-//const&
-int dist(Point a, Point b){
-  return sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
-}
+
 class PolygonalChain{
     public:
-        PolygonalChain(int sz_, Point *pnt_)
-        :sz(sz_)
-        {
-            pnt = new Point[sz_];
-            for(int i = 0;i < sz_;i++){
-                pnt[i] = pnt_[i];
-            }
-            closed = 0;
-        }
-        PolygonalChain(PolygonalChain const &other)
-        {
-        	sz = other.sz;
-            pnt = new Point[sz];
-            for(int i = 0;i < sz;i++){
-              pnt[i] = other.pnt[i];
-			}
-        }
-        PolygonalChain& operator=(const PolygonalChain& other){
-            if(&other == this){
-                return *this;
-            }
-            sz = other.sz;
-			delete[] pnt;
-            pnt = new Point[sz];
-            for(int i = 0;i < sz;i++){
-              pnt[i] = other.pnt[i];
-			}
-            return *this;
-        }
-        int getN()const{
-            return sz;
-        }
-        Point getPoint(int ind)const{
-            return pnt[ind];
-        }
-        int perimeter()const{
-            int res = 0;
-            for(int i = 1;i < getN();i++){
-                res += dist(pnt[i],pnt[i - 1]);
-            }
-            if(getClosed()) {
-                res += dist(pnt[0],pnt[getN() - 1]);
-            }
-            return res;
-        }
-        
-        void setClosed(bool val) {
-            closed = val;
-        }
-        bool getClosed() const {
-            return closed;
-        }
-        
-        ~PolygonalChain(){
-		delete[] pnt;
-		//why? hmmm
-		pnt = 0;
-    	}
+        PolygonalChain(int sz_, Point *pnt_);
+        PolygonalChain(PolygonalChain const &other);
+        PolygonalChain& operator=(const PolygonalChain& other);
+        int getN()const;
+        Point getPoint(int ind)const;
+        int perimeter()const;
+        void setClosed(bool val);
+        bool getClosed() const;
+        ~PolygonalChain();
     	
     protected:
         int sz;
         Point *pnt;
         bool closed;
 };
+
 class ClosedPolygonalChain: public PolygonalChain{
     public:
-        ClosedPolygonalChain(int sz, Point *pnt):  PolygonalChain(sz, pnt){
-            setClosed(1);
-        }
-    ~ClosedPolygonalChain(){
-    }
+        ClosedPolygonalChain(int sz, Point *pnt);
+        
+    ~ClosedPolygonalChain();
 };
+
 class Polygon: public ClosedPolygonalChain{
     public:
-        Polygon(int sz, Point *pnt) : ClosedPolygonalChain(sz, pnt) {
-        }
-        double area()const{
-            double S_Polygon = 0;
-            for(int i = 1;i < this->getN() - 1;i++){
-            	S_Polygon += s_triangle(this->getPoint(0),this->getPoint(i),this->getPoint(i + 1));
-            }
-            return S_Polygon;
-        }
-        ~Polygon(){
-    	}
+        Polygon(int sz, Point *pnt);
+        double area()const;
+        ~Polygon();
+        
     private:
-        double s_triangle(Point a,Point b, Point c)const{
-        	return abs((a.getX() - b.getX()) * (c.getY() - b.getY()) - (a.getY() - b.getY()) * (c.getX() - b.getX())) / 2;
-        }
+        double s_triangle(Point a,Point b, Point c)const;
 };
+
 class Triangle: public Polygon{
     public:
-        Triangle(int sz, Point *pnt) : Polygon(sz, pnt) {
-        }
-        bool hasRightAngle()const{
-            Point a = pnt[0];
-            Point b = pnt[1];
-            Point c = pnt[2];
-            //dist use sqrt, and here u use pow2
-            int ab = dist(a,b) * dist(a,b);
-            int bc = dist(b,c) * dist(b,c);
-            int ca = dist(c,a) * dist(c,a);
-            if(ab + bc ==  ca){
-                return true;
-            }
-            if(bc +  ca == ab){
-                return true;
-            }
-            if(ca +  ab ==  bc) {
-              return true;
-            }
-            return false;
-        }
-        ~Triangle(){
-    	}
+        Triangle(int sz, Point *pnt);
+        bool hasRightAngle()const;
+        ~Triangle();
         
 };
+
 class Trapezoid: public Polygon{
     public:
-        Trapezoid(int sz, Point *pnt): Polygon(sz, pnt) {
-        }
-        int height()const{
-            int S = area();
-            Point a = pnt[1];
-            Point b = pnt[2];
-            Point c = pnt[0];
-            Point d = pnt[3];
-            int foundation1 = dist(a,b);
-            int foundation2 = dist(c,d);
-            return 2 * S / (foundation1 + foundation2);
-        }
-        ~Trapezoid(){
-    	}
+        Trapezoid(int sz, Point *pnt);
+        int height()const;
+        ~Trapezoid();
 };
+
 class RegularPolygon: public Polygon{
     public:
-        RegularPolygon(int sz,Point* pnt): Polygon(sz,pnt){
-        }
-        int perimeter()const{
-		return getN() * dist(pnt[0], pnt[1]);
-	}
-	int area()const{
-		return (getN() * dist(pnt[0], pnt[1]) * dist(pnt[0], pnt[1]))  / (4. * tan(3.14 / getN()));
-	}
-        ~RegularPolygon(){
-    	}
+        RegularPolygon(int sz,Point* pnt);
+        int perimeter()const;
+        
+	int area()const;
+	
+    ~RegularPolygon();
 };
+
+int dist(const Point& a, const Point& b);
+
+int dist2(const Point& a, const Point& b);
+#endif
